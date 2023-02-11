58400a0c20a2e3c2ce5eaa730817e062b263b993
yezhiyi9670 <yezhiyi@toxic2018.ml>
2023-02-11 10:15:22 +0800

    Version number 1.0.1 - Add build for the (actual) umd format. - Converted non-essential fonts to woff to reduce size.

--- src/nmn/index.ts
+++ src/nmn/index.ts
@@ -57,9 +57,9 @@ class SparksNMNClass {
 			throw new NoRendererError('Sparks NMN renderer cannot work without a DOM window.')
 		}
 		const fonts = [
-			{ name: 'SimSun', url: './nmn/font/simsun/simsun.ttf', type: 'application/x-font-ttf' },
-			{ name: 'SimHei', url: './nmn/font/simhei/simhei.ttf', type: 'application/x-font-ttf' },
-			{ name: 'Deng', url: './nmn/font/deng/deng.ttf', type: 'application/x-font-ttf' },
+			{ name: 'SimSun', url: './nmn/font/simsun/simsun.woff', type: 'application/x-font-woff' },
+			{ name: 'SimHei', url: './nmn/font/simhei/simhei.woff', type: 'application/x-font-woff' },
+			{ name: 'Deng', url: './nmn/font/deng/deng.woff', type: 'application/x-font-woff' },
 			{ name: 'SparksNMN-EOPNumber', url: './nmn/font/eop_number/eop_number.ttf', type: 'application/x-font-ttf' },
 			{ name: 'SparksNMN-mscore-20', url: './nmn/font/mscore-20/mscore-20.ttf', type: 'application/x-font-ttf' },
 			{ name: 'SparksNMN-Bravura', url: './nmn/font/bravura/bravura.woff', type: 'application/x-font-woff' }
