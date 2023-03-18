3efd33f00fc36aa008454a8588603e168e6dccd0
yezhiyi9670 <yezhiyi@toxic2018.ml>
2023-03-18 21:50:54 +0800

    Lowered editor

--- src/app/DemoEditor/IntegratedEditor.tsx
+++ src/app/DemoEditor/IntegratedEditor.tsx
@@ -21,7 +21,7 @@ const useStyles = createUseStyles({
 	},
 	groupPreview: {
 		height: 0,
-		flex: 5,
+		flex: 4,
 		overflowY: 'auto'
 	},
 	groupSeparator: {
@@ -30,7 +30,7 @@ const useStyles = createUseStyles({
 	},
 	groupEdit: {
 		height: 0,
-		flex: 4,
+		flex: 3,
 	},
 	groupStatusBar: {
 		borderTop: '1px solid #0002',
