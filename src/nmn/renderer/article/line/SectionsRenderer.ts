7ee853d87c0f9298e2ba8c8e068f880ca468415b
yezhiyi9670 <yezhiyi@toxic2018.ml>
2023-03-19 09:42:04 +0800

    Further raised custom triplet

--- src/nmn/renderer/article/line/SectionsRenderer.ts
+++ src/nmn/renderer/article/line/SectionsRenderer.ts
@@ -274,7 +274,7 @@ export class SectionsRenderer {
 							const endX = this.columns.fracPosition(sectionIndex, Frac.add(section.startPos, decor.endPos)) + noteMeasure[0] / 2
 							// const lowY = currY - noteMeasure[1] / 2 - 0.5
 							let highY = currY - noteMeasure[1] / 2 - 2
-							const heightSpacing = noteMeasure[1] * 0.25
+							const heightSpacing = noteMeasure[1] * (tripletText == '3' ? 0.25 : 0.4)
 							checkNoteList((note) => {
 								highY = Math.min(highY, note.leftTop - heightSpacing)
 								highY = Math.min(highY, note.rightTop - heightSpacing)
