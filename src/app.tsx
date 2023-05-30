87e4952fc570d1869061af1c20721cf52f2d1e42
yezhiyi9670 <yezhiyi@toxic2018.ml>
2023-05-30 20:12:18 +0800

    Remove console tests

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 
 import { SparksNMN } from './nmn'
 import { Parser } from './nmn/parser/parser'
-import nmnExamples from './nmn/examples/examples'
 import { Frac } from './nmn/util/frac'
 import { MusicTheory } from './nmn/util/music'
 import { md5 } from './nmn/util/md5'
@@ -11,33 +10,33 @@ import { TestApp } from './app/TestApp'
 import { I18nProvider } from './app/i18n/i18n'
 
 function ConsoleTest() {
-	console.log(md5('abc'))
-	console.log('Complicated')
-	console.log(Parser.parse(nmnExamples.structure_complicated))
-	console.log('Multisection')
-	console.log(Parser.parse(nmnExamples.strcture_multiSection))
-	console.log('Multipart')
-	console.log(Parser.parse(nmnExamples.basics_multiPart))
-	console.log('Fracs')
-	console.log(Frac.repr(Frac.create(3, 2)))
-	console.log(Frac.repr(Frac.__sum(Frac.create(1, 2), Frac.create(1, 6), Frac.create(1, 3))))
-	console.log(Frac.repr(Frac.prod(Frac.create(1, 2), Frac.create(2, 3), Frac.create(3, 4))))
-	console.log('Abs Names')
-	console.log(MusicTheory.absName2Pitch('C4'))
-	console.log(MusicTheory.absName2Pitch('C5'))
-	console.log(MusicTheory.absName2Pitch('C3'))
-	console.log(MusicTheory.absName2Pitch('##C4'))
-	console.log(MusicTheory.absName2Pitch('#B4'))
-	console.log(MusicTheory.absName2Pitch('C13'))
-	console.log('Degrees')
-	console.log(MusicTheory.pitchInterval2dKey(1, 'th'))
-	console.log(MusicTheory.pitchInterval2dKey(1, 'thd'))
-	console.log(MusicTheory.pitchInterval2dKey(1, 'thm'))
-	console.log(MusicTheory.pitchInterval2dKey(4, 'thm'))
-	console.log(MusicTheory.pitchInterval2dKey(5, 'thm'))
-	console.log(MusicTheory.pitchInterval2dKey(8, 'th'))
-	console.log(MusicTheory.pitchInterval2dKey(-8, 'th'))
-	return <></>
+	// console.log(md5('abc'))
+	// console.log('Complicated')
+	// console.log(Parser.parse(nmnExamples.structure_complicated))
+	// console.log('Multisection')
+	// console.log(Parser.parse(nmnExamples.strcture_multiSection))
+	// console.log('Multipart')
+	// console.log(Parser.parse(nmnExamples.basics_multiPart))
+	// console.log('Fracs')
+	// console.log(Frac.repr(Frac.create(3, 2)))
+	// console.log(Frac.repr(Frac.__sum(Frac.create(1, 2), Frac.create(1, 6), Frac.create(1, 3))))
+	// console.log(Frac.repr(Frac.prod(Frac.create(1, 2), Frac.create(2, 3), Frac.create(3, 4))))
+	// console.log('Abs Names')
+	// console.log(MusicTheory.absName2Pitch('C4'))
+	// console.log(MusicTheory.absName2Pitch('C5'))
+	// console.log(MusicTheory.absName2Pitch('C3'))
+	// console.log(MusicTheory.absName2Pitch('##C4'))
+	// console.log(MusicTheory.absName2Pitch('#B4'))
+	// console.log(MusicTheory.absName2Pitch('C13'))
+	// console.log('Degrees')
+	// console.log(MusicTheory.pitchInterval2dKey(1, 'th'))
+	// console.log(MusicTheory.pitchInterval2dKey(1, 'thd'))
+	// console.log(MusicTheory.pitchInterval2dKey(1, 'thm'))
+	// console.log(MusicTheory.pitchInterval2dKey(4, 'thm'))
+	// console.log(MusicTheory.pitchInterval2dKey(5, 'thm'))
+	// console.log(MusicTheory.pitchInterval2dKey(8, 'th'))
+	// console.log(MusicTheory.pitchInterval2dKey(-8, 'th'))
+	// return <></>
 }
 
 function App() {
